1464cdd0b8ce71f48350e5dc2d5ca7ef2a134ed4
Daan <47692094+Daaninator@users.noreply.github.com>
2025-12-19 18:47:43 +0100

    feat: added mouse clicking noise

--- portfolio/app/mainPage.tsx
+++ portfolio/app/mainPage.tsx
@@ -2,7 +2,7 @@
 import mainPageStyles from './css/mainPage.module.css';
 import crtStyles from './css/crt.module.css';
 import './css/global.css';
-import React, { useState, useEffect, Dispatch, SetStateAction } from 'react';
+import React, { useState, useEffect, Dispatch, SetStateAction, useRef } from 'react';
 import Image from "next/image";
 import dynamic from 'next/dynamic';
 import { Geist, Geist_Mono } from "next/font/google";
@@ -26,12 +26,42 @@ const PreloadContent = dynamic(() => import('./page'), {
 });
 
 
+
 export default function Mainpage() {
   const [maximizeState, setMaximizeState] = useState(false);
 
+  const mouseClickSoundRef = useRef<HTMLAudioElement | null>(null);
+  const startUpSoundRef = useRef<HTMLAudioElement | null>(null);
+
+  // For mouse clicks
+  useEffect(() => {
+    const playSound = () => {
+      if (mouseClickSoundRef.current) {
+        mouseClickSoundRef.current.volume = 0.2;
+        mouseClickSoundRef.current.currentTime = 0;
+        mouseClickSoundRef.current.play().catch(err => { });
+      }
+    };
+    // Attach listener
+    window.addEventListener('click', playSound);
+    // Cleanup 
+    return () => window.removeEventListener('click', playSound);
+  }, []);
+
+  // For start up sound effect
+  // Doesnt work because user needs to interact with screen before laying sound :(
+  useEffect(() => {
+    if (startUpSoundRef.current) {
+      startUpSoundRef.current.volume = 0.2;
+      startUpSoundRef.current.currentTime = 0;
+      startUpSoundRef.current.play().catch(err => { });
+    };
+  }, []);
+
   return (
     <body className={`${mainPageStyles.screen} ${crtStyles.crtFishEye}`}>
-
+      <audio ref={mouseClickSoundRef} src="/mouse-click-2.mp3" preload="auto" />
+      <audio ref={startUpSoundRef} src="/windows-xp-startup.mp3" preload="auto" />
 
       <main className={`${mainPageStyles.content} `}>
         <div className={`${crtStyles.monitor}`}>
