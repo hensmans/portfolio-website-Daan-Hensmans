8be40722b7b8fa21cb5ca3f60a068b43571c5612
Daan <47692094+Daaninator@users.noreply.github.com>
2025-12-19 19:07:55 +0100

    feat: added flickering

--- portfolio/app/mainPage.tsx
+++ portfolio/app/mainPage.tsx
@@ -60,10 +60,11 @@ export default function Mainpage() {
       <main className={`${mainPageStyles.content} `}>
         <div className={`${crtStyles.monitor}`}>
           <Image className={`${crtStyles.monitorScreen} noMousePointer`} fill priority alt='Monitor border screen' src="/monitor-screen-border-5.png" />
-          <Image className={crtStyles.monitorName} fill priority alt='Monitor border screen' src="/monitor-screen-daan-hensmans-3.png" />
+          <Image className={crtStyles.monitorName} fill priority alt='Monitor border screen' src="/monitor-screen-daan-hensmans-8.png" />
         </div>
-        <div className={` ${crtStyles.crt}  ${crtStyles.crtMainScreen} ${crtStyles.crtLines} mousePointer ${mainPageStyles.windowsXPBackground}`}
+        <div className={` ${crtStyles.crt}  ${crtStyles.crtMainScreen} mousePointer ${mainPageStyles.windowsXPBackground}`}
           onMouseDown={handleMouseClickSoundEffect}>
+          <div className={crtStyles.crtLines} />
           <PreloadContent setMaximizeState={setMaximizeState} maximizeState={maximizeState} />
           <Taskbar />
           <LoadingStartpage />
