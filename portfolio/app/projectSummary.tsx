3bfdcb1d04b5e7b6afafd4a33e37826bf6d3ff44
Daan <47692094+Daaninator@users.noreply.github.com>
2025-12-20 15:26:08 +0100

    feat: beautified file structure for projects

---
+++ portfolio/app/projectSummary.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import popupButtonStyles from './css/popupButton.module.css';
+import { Dispatch, SetStateAction } from "react";
+import './css/global.css';
+import './css/global.css'
+
+interface Parameters {
+}
+
+const ProjectSummary = () => {
+    return (
+        <div >
+
+        </div>
+
+
+    );
+}
+export default ProjectSummary;
+
--- portfolio/app/projects.tsx
+++ portfolio/app/projects.tsx
@@ -1,49 +1,111 @@
 
+import { useState } from 'react';
 import './css/global.css';
 import projectsStyles from './css/projects.module.css';
 
 const Projects = () => {
+
+
+    const items = [
+        {
+            folderName: 'root',
+            folderFiles: [
+                { id: 0, label: 'README.md' }
+            ]
+        },
+        {
+            folderName: 'games',
+            folderFiles: [
+                { id: 1, label: 'Dr Mario' },
+                { id: 2, label: 'Worm' },
+                { id: 3, label: 'Desert Explorer' },
+                { id: 4, label: 'Dr Mario' },
+                { id: 5, label: 'Train App' },
+                { id: 6, label: 'Pixel Simulator' }
+            ]
+        },
+        {
+            folderName: 'low level',
+            folderFiles: [
+                { id: 10, label: 'HPC in C' },
+                { id: 11, label: 'OpenCL' },
+                { id: 12, label: 'OpenGL' },
+                { id: 13, label: 'EmFRP Compiler' },
+                { id: 14, label: 'Interpreter' },
+            ]
+        },
+        {
+            folderName: 'creative',
+            folderFiles: [
+                { id: 21, label: 'This Site' },
+                { id: 22, label: 'Photography' },
+                { id: 23, label: 'YouTube' },
+            ]
+        },
+        {
+            folderName: 'constructs',
+            folderFiles: [
+                { id: 31, label: 'Parallellism' },
+                { id: 32, label: 'Database' },
+                { id: 33, label: 'Multicore' },
+                { id: 34, label: 'Web Security' },
+            ]
+        },
+    ];
+
+    const generateFolderContent = (fileName: string) => {
+        const folder = items.find(entry => entry.folderName === fileName);
+        return (
+            folder?.folderFiles.map((file) => (
+                <li
+                    key={file.id}
+                    onClick={() => setSelectedFile(file.id)}
+                    // 2. Conditionally apply the "selected" class
+                    className={`${selectedFile === file.id ? `${projectsStyles.fileActive}` : ''}`}
+                >
+                    {file.label}
+                </li>
+            ))
+        );
+    }
+
+    const generateFolder = (openBool: boolean, title: string, fileName: string) => {
+        return (
+            <details open={openBool}>
+                <summary>{title}</summary>
+                <ul>
+                    {generateFolderContent(fileName)}
+                </ul>
+            </details>
+        );
+    }
+
+
+
+
+    // Readme is always selected
+    const [selectedFile, setSelectedFile] = useState(0);
+
     return (
         <div className={`${projectsStyles.layout}`}>
-            <ul className={`${projectsStyles.treeView} ${projectsStyles.tree}`}>
-                <li >PROJECTS</li>
+            <ul className={`${projectsStyles.tree}`}>
                 <li >
                     <details open>
-                        <summary>Games</summary>
+                        <summary>Local Disk (C:)</summary>
                         <ul>
-                            <li>Dr Mario</li>
-                            <li>Worm</li>
-                            <li>Desert Explorer</li>
-                            <li>Dr Mario</li>
-                            <li>Train App</li>
-                            <li>Pixel Simulator</li>
+                            <details open>
+                                <summary>projects</summary>
+                                <ul>
+                                    {generateFolderContent('root')}
+                                    <li >
+                                        {generateFolder(true, 'games', 'games')}
+                                        {generateFolder(false, 'low level', 'low level')}
+                                        {generateFolder(false, 'constructs', 'constructs')}
+                                        {generateFolder(false, 'creative', 'creative')}
+                                    </li>
+                                </ul>
+                            </details>
                         </ul>
-                    </details>
-                    <details open>
-                        <summary>Low level</summary>
-                        <ul>
-                            <li>C opptimizations</li>
-                            <li>OPENCL</li>
-                            <li>OPENGL</li>
-                            <li>EmFRP Compiler</li>
-                            <li>Interpreter</li>
-                        </ul>
-                    </details>
-                    <details open>
-                        <summary>Constructs</summary>
-                        <ul>
-                            <li>Parallellism</li>
-                            <li>Database</li>
-                            <li>Multicore</li>
-                            <li>Database</li>
-                            <li>Web Security</li>
-                        </ul>
-                    </details>
-                    <details open>
-                        <summary>Creativity</summary>
-                        <ul>This site</ul>
-                        <ul>YouTube</ul>
-                        <ul>Photography</ul>
                     </details>
                 </li>
             </ul>
