2ef3dd1c1d89231bbf756baaa8daa542b09dab2f
Daan <Daan.hensmans@gmail.com>
2026-01-06 02:53:43 +0100

    fix: no popup icon on web

--- portfolio/app/buttonSelection.tsx
+++ portfolio/app/buttonSelection.tsx
@@ -3,6 +3,11 @@ import PopupButton from './popupButton';
 import './css/global.css';
 import buttonSelectionStyles from './css/buttonSelection.module.css';
 
+import projects from '../assets/icons/projects.webp';
+import pictures from '../assets/icons/pictures.webp';
+import aboutme from '../assets/icons/aboutme.webp';
+import cv from '../assets/icons/cv.webp';
+
 interface ButtonSelectionProps {
   setSelectedButton: Dispatch<SetStateAction<string>>;
 }
@@ -11,10 +16,10 @@ const ButtonSelection = ({ setSelectedButton }: ButtonSelectionProps) => {
 
   return (
     <div className={buttonSelectionStyles.buttonsBox}>
-      <PopupButton setSelectedButton={setSelectedButton} iconName={'aboutme'} title={'aboutMe.html'} />
-      <PopupButton setSelectedButton={setSelectedButton} iconName={'cv'} title={'cv.pdf'} />
-      <PopupButton setSelectedButton={setSelectedButton} iconName={'projects'} title={'projects'} />
-      <PopupButton setSelectedButton={setSelectedButton} iconName={'pictures'} title={'pictures'} />
+      <PopupButton setSelectedButton={setSelectedButton} icon={aboutme} iconName={'aboutme'} title={'aboutMe.html'} />
+      <PopupButton setSelectedButton={setSelectedButton} icon={cv} iconName={'cv'} title={'cv.pdf'} />
+      <PopupButton setSelectedButton={setSelectedButton} icon={projects} iconName={'projects'} title={'projects'} />
+      <PopupButton setSelectedButton={setSelectedButton} icon={pictures} iconName={'pictures'} title={'pictures'} />
     </div>
   );
 }
--- portfolio/app/popupButton.tsx
+++ portfolio/app/popupButton.tsx
@@ -2,22 +2,24 @@ import Image from "next/image";
 import popupButtonStyles from './css/popupButton.module.css';
 import { Dispatch, SetStateAction } from "react";
 import './css/global.css';
-import './css/global.css'
-interface ButtonSelectionProps {
+import { StaticImageData } from 'next/image';
+
+interface Parameters {
     setSelectedButton: Dispatch<SetStateAction<string>>;
     iconName: string;
+    icon: StaticImageData;
     title: string;
 }
 
-const PopupButton = ({ setSelectedButton, iconName, title }: ButtonSelectionProps) => {
+const PopupButton = ({ setSelectedButton, iconName, title, icon }: Parameters) => {
     return (
         <div className={`${popupButtonStyles.buttonBox} xp-icon-label noSelect clickable`} onClick={() => setSelectedButton(iconName)}>
             <div className={`${popupButtonStyles.iconBox}`}>
-                <Image src={`/icons/${iconName}.webp`}
+                <Image src={icon}
                     alt={`Button to ${iconName}`}
                     fill
                     className={`${popupButtonStyles.clickableImage}`}
-                    priority // preloads
+                    priority
                 />
             </div>
             <p className={`${popupButtonStyles.iconFont}`}>{title}</p>
